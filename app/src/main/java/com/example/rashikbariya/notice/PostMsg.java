d2599d4ba887a05816d3f727a7ed8b3b2677d755
Rashik <kumar7rashik@gmail.com>
2018-06-12 22:13:34 +0545

    admin page is added

---
+++ app/src/main/java/com/example/rashikbariya/notice/PostMsg.java
@@ -0,0 +1,13 @@
+package com.example.rashikbariya.notice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class PostMsg extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post);
+    }
+}
